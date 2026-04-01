a8f7834434937f1b58145ac0bab995e60f5d0237
Samuel Ramon <samuelrbo@gmail.com>
2026-04-01 22:54:30 +0100

    feat: :truck: move scripts to folder

--- scripts/build-extension.js
+++ scripts/build-extension.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const archiver = require('archiver');
 
-const sourceDir = path.join(__dirname, 'extension');
-const outPath = path.join(__dirname, 'extension.zip');
+const sourceDir = path.join(__dirname, '..', 'extension');
+const outPath = path.join(__dirname, '..', 'extension.zip');
 
 const output = fs.createWriteStream(outPath);
 const archive = archiver('zip', { zlib: { level: 9 } });
